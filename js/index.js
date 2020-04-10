3672873889d2b10f7dd7fea6fe501a2447627586
Kyrune <jn-01@centurytel.net>
2020-04-10 11:40:46 -0500

    Add "Back to Top" JS

--- js/index.js
+++ js/index.js
@@ -10,3 +10,21 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+// Scroll to top functionality
+topButton = document.getElementById('topBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = 'block';
+    } else {
+        topButton.style.display = 'none';
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
